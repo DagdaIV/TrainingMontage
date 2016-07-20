055d1b97447e8a8a754f632ef70f22ecbf732140
DagdaIV <dagdaiv@gmail.com>
2016-07-19 22:28:36 -0600

    Finally should be done.

--- Shoyeido Work Calculator/Shoyeido Work Calculator/Form2.Designer.cs
+++ Shoyeido Work Calculator/Shoyeido Work Calculator/Form2.Designer.cs
@@ -35,21 +35,23 @@
             this.dateTimePicker5 = new System.Windows.Forms.DateTimePicker();
             this.dateTimePicker6 = new System.Windows.Forms.DateTimePicker();
             this.SetBut = new System.Windows.Forms.Button();
+            this.ClearBut = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // dateTimePicker1
             // 
             this.dateTimePicker1.Location = new System.Drawing.Point(12, 31);
             this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.Size = new System.Drawing.Size(99, 20);
             this.dateTimePicker1.TabIndex = 0;
             this.dateTimePicker1.Value = new System.DateTime(2016, 7, 13, 21, 35, 15, 0);
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
             // 
             // dateTimePicker2
             // 
-            this.dateTimePicker2.Location = new System.Drawing.Point(218, 31);
+            this.dateTimePicker2.Location = new System.Drawing.Point(127, 31);
             this.dateTimePicker2.Name = "dateTimePicker2";
-            this.dateTimePicker2.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker2.Size = new System.Drawing.Size(99, 20);
             this.dateTimePicker2.TabIndex = 1;
             this.dateTimePicker2.Value = new System.DateTime(2016, 7, 13, 21, 36, 21, 0);
             this.dateTimePicker2.ValueChanged += new System.EventHandler(this.dateTimePicker2_ValueChanged);
@@ -58,16 +60,17 @@
             // 
             this.dateTimePicker3.Location = new System.Drawing.Point(12, 57);
             this.dateTimePicker3.Name = "dateTimePicker3";
-            this.dateTimePicker3.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker3.Size = new System.Drawing.Size(99, 20);
             this.dateTimePicker3.TabIndex = 2;
             this.dateTimePicker3.Value = new System.DateTime(2016, 7, 13, 21, 36, 31, 0);
             this.dateTimePicker3.Visible = false;
+            this.dateTimePicker3.ValueChanged += new System.EventHandler(this.dateTimePicker3_ValueChanged);
             // 
             // dateTimePicker4
             // 
-            this.dateTimePicker4.Location = new System.Drawing.Point(218, 57);
+            this.dateTimePicker4.Location = new System.Drawing.Point(127, 57);
             this.dateTimePicker4.Name = "dateTimePicker4";
-            this.dateTimePicker4.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker4.Size = new System.Drawing.Size(99, 20);
             this.dateTimePicker4.TabIndex = 3;
             this.dateTimePicker4.Value = new System.DateTime(2016, 7, 13, 21, 36, 38, 0);
             this.dateTimePicker4.Visible = false;
@@ -77,16 +80,17 @@
             // 
             this.dateTimePicker5.Location = new System.Drawing.Point(12, 83);
             this.dateTimePicker5.Name = "dateTimePicker5";
-            this.dateTimePicker5.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker5.Size = new System.Drawing.Size(99, 20);
             this.dateTimePicker5.TabIndex = 4;
             this.dateTimePicker5.Value = new System.DateTime(2016, 7, 13, 21, 36, 43, 0);
             this.dateTimePicker5.Visible = false;
+            this.dateTimePicker5.ValueChanged += new System.EventHandler(this.dateTimePicker5_ValueChanged);
             // 
             // dateTimePicker6
             // 
-            this.dateTimePicker6.Location = new System.Drawing.Point(218, 83);
+            this.dateTimePicker6.Location = new System.Drawing.Point(127, 83);
             this.dateTimePicker6.Name = "dateTimePicker6";
-            this.dateTimePicker6.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker6.Size = new System.Drawing.Size(99, 20);
             this.dateTimePicker6.TabIndex = 5;
             this.dateTimePicker6.Value = new System.DateTime(2016, 7, 13, 21, 36, 49, 0);
             this.dateTimePicker6.Visible = false;
@@ -94,7 +98,7 @@
             // 
             // SetBut
             // 
-            this.SetBut.Location = new System.Drawing.Point(173, 109);
+            this.SetBut.Location = new System.Drawing.Point(36, 109);
             this.SetBut.Name = "SetBut";
             this.SetBut.Size = new System.Drawing.Size(75, 23);
             this.SetBut.TabIndex = 6;
@@ -102,11 +106,22 @@
             this.SetBut.UseVisualStyleBackColor = true;
             this.SetBut.Click += new System.EventHandler(this.SetBut_Click);
             // 
+            // ClearBut
+            // 
+            this.ClearBut.Location = new System.Drawing.Point(127, 108);
+            this.ClearBut.Name = "ClearBut";
+            this.ClearBut.Size = new System.Drawing.Size(75, 23);
+            this.ClearBut.TabIndex = 7;
+            this.ClearBut.Text = "Clear";
+            this.ClearBut.UseVisualStyleBackColor = true;
+            this.ClearBut.Click += new System.EventHandler(this.ClearBut_Click);
+            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(427, 169);
+            this.ClientSize = new System.Drawing.Size(251, 169);
+            this.Controls.Add(this.ClearBut);
             this.Controls.Add(this.SetBut);
             this.Controls.Add(this.dateTimePicker6);
             this.Controls.Add(this.dateTimePicker5);
@@ -130,5 +145,6 @@
         private System.Windows.Forms.DateTimePicker dateTimePicker5;
         private System.Windows.Forms.DateTimePicker dateTimePicker6;
         private System.Windows.Forms.Button SetBut;
+        private System.Windows.Forms.Button ClearBut;
     }
 }
--- Shoyeido Work Calculator/Shoyeido Work Calculator/Form2.cs
+++ Shoyeido Work Calculator/Shoyeido Work Calculator/Form2.cs
@@ -14,10 +14,23 @@ namespace Shoyeido_Work_Calculator
     public partial class Form2 : Form
     {
         public string[] datevals = new string[3];
+        String dateTimePickerFormat = "MM/dd/yyyy";
         string docloc = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
         public Form2()
         {
             InitializeComponent();
+            this.dateTimePicker1.CustomFormat = " ";
+            this.dateTimePicker1.Format = DateTimePickerFormat.Custom;
+            this.dateTimePicker2.CustomFormat = " ";
+            this.dateTimePicker2.Format = DateTimePickerFormat.Custom;
+            this.dateTimePicker3.CustomFormat = " ";
+            this.dateTimePicker3.Format = DateTimePickerFormat.Custom;
+            this.dateTimePicker4.CustomFormat = " ";
+            this.dateTimePicker4.Format = DateTimePickerFormat.Custom;
+            this.dateTimePicker5.CustomFormat = " ";
+            this.dateTimePicker5.Format = DateTimePickerFormat.Custom;
+            this.dateTimePicker6.CustomFormat = " ";
+            this.dateTimePicker6.Format = DateTimePickerFormat.Custom;
         }
 
         public DateTime ConvDate(string date)
@@ -33,22 +46,28 @@ namespace Shoyeido_Work_Calculator
             datevals[1] = "";
             datevals[2] = "";
             ptodates = File.ReadAllLines(@Path.Combine(docloc, "pto.txt"));
-            for(byte count = 0; count<2; count++)
+            for(byte count = 0; count<=2; count++)
             {
                 if (!ptodates[count].Equals(""))
                 {
                     if (count == 0)
                     {
+                        this.dateTimePicker1.CustomFormat = dateTimePickerFormat;
+                        this.dateTimePicker2.CustomFormat = dateTimePickerFormat;
                         dateTimePicker1.Value = ConvDate(ptodates[count].Substring(0, 8));
                         dateTimePicker2.Value = ConvDate(ptodates[count].Substring(8, 8));
                     }
                     if (count == 1)
                     {
+                        this.dateTimePicker3.CustomFormat = dateTimePickerFormat;
+                        this.dateTimePicker4.CustomFormat = dateTimePickerFormat;
                         dateTimePicker3.Value = ConvDate(ptodates[count].Substring(0, 8));
                         dateTimePicker4.Value = ConvDate(ptodates[count].Substring(8, 8));
                     }
                     if (count == 2)
                     {
+                        this.dateTimePicker5.CustomFormat = dateTimePickerFormat;
+                        this.dateTimePicker6.CustomFormat = dateTimePickerFormat;
                         dateTimePicker5.Value = ConvDate(ptodates[count].Substring(0, 8));
                         dateTimePicker6.Value = ConvDate(ptodates[count].Substring(8, 8));
                     }
@@ -58,6 +77,7 @@ namespace Shoyeido_Work_Calculator
 
         private void dateTimePicker4_ValueChanged(object sender, EventArgs e)
         {
+            this.dateTimePicker4.CustomFormat = dateTimePickerFormat;
             datevals[1] = dateTimePicker3.Value.ToString("yyyyMMdd") + dateTimePicker4.Value.ToString("yyyyMMdd");
             dateTimePicker5.Visible = true;
             dateTimePicker6.Visible = true;
@@ -65,6 +85,7 @@ namespace Shoyeido_Work_Calculator
 
         private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
         {
+            this.dateTimePicker2.CustomFormat = dateTimePickerFormat;
             datevals[0] = dateTimePicker1.Value.ToString("yyyyMMdd") + dateTimePicker2.Value.ToString("yyyyMMdd");
             dateTimePicker3.Visible = true;
             dateTimePicker4.Visible = true;
@@ -72,12 +93,48 @@ namespace Shoyeido_Work_Calculator
 
         private void dateTimePicker6_ValueChanged(object sender, EventArgs e)
         {
+            this.dateTimePicker6.CustomFormat = dateTimePickerFormat;
             datevals[2] = dateTimePicker5.Value.ToString("yyyyMMdd") + dateTimePicker6.Value.ToString("yyyyMMdd");
         }
 
         private void SetBut_Click(object sender, EventArgs e)
         {
             File.WriteAllLines(@Path.Combine(docloc, "pto.txt"), datevals);
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            this.dateTimePicker1.CustomFormat = dateTimePickerFormat;
+        }
+
+        private void dateTimePicker3_ValueChanged(object sender, EventArgs e)
+        {
+            this.dateTimePicker3.CustomFormat = dateTimePickerFormat;
+        }
+
+        private void dateTimePicker5_ValueChanged(object sender, EventArgs e)
+        {
+            this.dateTimePicker5.CustomFormat = dateTimePickerFormat;
+        }
+
+        private void ClearBut_Click(object sender, EventArgs e)
+        {
+            this.dateTimePicker1.CustomFormat = " ";
+            this.dateTimePicker1.Format = DateTimePickerFormat.Custom;
+            this.dateTimePicker2.CustomFormat = " ";
+            this.dateTimePicker2.Format = DateTimePickerFormat.Custom;
+            this.dateTimePicker3.CustomFormat = " ";
+            this.dateTimePicker3.Format = DateTimePickerFormat.Custom;
+            this.dateTimePicker4.CustomFormat = " ";
+            this.dateTimePicker4.Format = DateTimePickerFormat.Custom;
+            this.dateTimePicker5.CustomFormat = " ";
+            this.dateTimePicker5.Format = DateTimePickerFormat.Custom;
+            this.dateTimePicker6.CustomFormat = " ";
+            this.dateTimePicker6.Format = DateTimePickerFormat.Custom;
+            dateTimePicker3.Visible = false;
+            dateTimePicker4.Visible = false;
+            dateTimePicker5.Visible = false;
+            dateTimePicker6.Visible = false;
         }
     }
 }
